2187e103cbfbf79601aec5a63f3fa4aee38d6513
thelacker <dyatlikos3000@ya.ru>
2016-02-24 10:23:21 +0300

    Added csv reader

--- DZ1/main.py
+++ DZ1/main.py
@@ -1,10 +1,16 @@
-import numpy
-import matplotlib
-import pandas
-import scipy
+#import numpy
+#import matplotlib
+#import pandas
+#import scipy
+import csv
+
+file_name = "2016.02.20.vyygrusska.csv"
 
 def main():
-    pass
+    reader = csv.reader(open(file_name, "rb"), delimiter=';')
+    for row in reader:
+        print row[25:28]
+        print row[30]
 
 if __name__ == "__main__":
     main()
