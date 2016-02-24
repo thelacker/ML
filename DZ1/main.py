06e84482ad740afe3d3cd6cae12f4a84ca7053dd
thelacker <dyatlikos3000@ya.ru>
2016-02-24 10:45:42 +0300

    Some new functions and comments Added exception checker

--- DZ1/main.py
+++ DZ1/main.py
@@ -4,13 +4,27 @@
 #import scipy
 import csv
 
-file_name = "2016.02.20.vyygrusska.csv"
 
 def main():
+    file_name = "2016.02.20.vyygrusska.csv"
+    p1_fraud = []
+    p2_fraud = []
+    p3_fraud = []
+    read_csv(file_name,p1_fraud,p2_fraud,p3_fraud)
+
+
+# Read csv file and make tuples of different frauds
+def read_csv(file_name, p1_fraud, p2_fraud, p3_fraud):
     reader = csv.reader(open(file_name, "rb"), delimiter=';')
+    next(reader)
     for row in reader:
-        print row[25:28]
-        print row[30]
+        try:
+            p1_fraud.append([row[25], row[30]])
+            p2_fraud.append([row[26], row[30]])
+            p3_fraud.append([row[27], row[30]])
+        except IndexError:
+            pass
+
 
 if __name__ == "__main__":
     main()
