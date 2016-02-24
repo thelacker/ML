93befd842233f567377e661bb42eaef120e305a6
thelacker <dyatlikos3000@ya.ru>
2016-02-24 11:00:03 +0300

    Some improvements

--- DZ1/main.py
+++ DZ1/main.py
@@ -6,24 +6,36 @@ import csv
 
 
 def main():
+    # File with data
     file_name = "2016.02.20.vyygrusska.csv"
+
+    # Different Magic Boxes
     p1_fraud = []
     p2_fraud = []
     p3_fraud = []
+
     read_csv(file_name,p1_fraud,p2_fraud,p3_fraud)
 
 
 # Read csv file and make tuples of different frauds
 def read_csv(file_name, p1_fraud, p2_fraud, p3_fraud):
+    # Reading file and kipping the header
     reader = csv.reader(open(file_name, "rb"), delimiter=';')
     next(reader)
+
     for row in reader:
         try:
             p1_fraud.append([row[25], row[30]])
             p2_fraud.append([row[26], row[30]])
             p3_fraud.append([row[27], row[30]])
         except IndexError:
+            # There are some broken rows, we have to skip them
             pass
+
+
+# Counting tp, pn, fp, fn
+def stats(fraud):
+    all_cases = fraud
 
 
 if __name__ == "__main__":
