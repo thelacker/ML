e79115642b98b2fa207bb13d2eed62167c3e14aa
thelacker <dyatlikos3000@ya.ru>
2016-02-24 11:48:15 +0300

    New way of counting tp, fp, tn, fn

--- DZ1/main.py
+++ DZ1/main.py
@@ -37,32 +37,30 @@ def read_csv(file_name, p1_fraud, p2_fraud, p3_fraud):
 
 # Counting tp, pn, fp, fn
 def stats(cases, threshold):
-    all_cases = len(cases)
-    tp_cases = 0
-    tn_cases = 0
-    fp_cases = 0
-    fn_cases = 0
+    all_cases = frod_cases = granted_cases = tp_cases = tn_cases = fp_cases = fn_cases = 0
     for case in cases:
-        # If the result is undefined - skip case
-        if case[1] == 'U':
-            continue
-
         # if the probability is bigger then threshold
-        # and case was Granted - then the Magic Box is right
-        if case[0] >= threshold and case[1] == 'F':
-            tp_cases += 1
-        elif case[0] >= threshold and case[1] == 'G':
-            fp_cases += 1
-        elif case[0] < threshold and case[1] == 'F':
-            fn_cases += 1
-        elif case[0] < threshold and case[1] == 'G':
-            tn_cases += 1
+        # and case was Frod - then the Magic Box is right
+        if case[1] == 'F':
+            frod_cases += 1
+            if float(case[0]) >= threshold:
+                tp_cases += 1
+        # if the probability is bigger then threshold
+        # and case was Granted - then the Magic Box is false
+        elif case[1] == "G":
+            granted_cases += 1
+            if float(case[0]) >= threshold:
+                fp_cases += 1
+        # If the result is undefined - skip case
+        else:
+            continue
+    all_cases = frod_cases + granted_cases
 
     print all_cases
+    print frod_cases
+    print granted_cases
     print tp_cases
-    print tn_cases
     print fp_cases
-    print fn_cases
 
 
 if __name__ == "__main__":
